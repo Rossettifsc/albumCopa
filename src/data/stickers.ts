d57693a4dfea02832c3dd27f4dd6c04d1e350a16
elisa.fonseca@alunos.sc.senac.br <elisa.fonseca@alunos.sc.senac.br>
2026-06-03 15:34:41 -0300

    ok

--- src/data/stickers.ts
+++ src/data/stickers.ts
@@ -15,6 +15,6 @@ export const initialStickers: Sticker[] = [
   { id: 6, name: 'Vinícius Jr', team: 'Brasil', photo: 'https://tse4.mm.bing.net/th/id/OIP.aA0LN6kbqGuDJsKfR60Q4wHaKk?rs=1&pid=ImgDetMain&o=7&rm=3', collected: false },
   { id: 7, name: 'Robert Lewandowski', team: 'Polônia', photo: 'https://a.allegroimg.com/original/11dd4d/fa50e98e467892b1293c8a079a1c/PANINI-FIFA-365-2026-FANS-FAVOURITE-blue-FAN73-ROBERT-LEWANDOWSKI-BARCELONA', collected: false },
   { id: 8, name: 'Luka Modric', team: 'Croácia', photo: 'https://tse4.mm.bing.net/th/id/OIP.a5w5lZJrZdUZNjRMPW0lSAHaJw?rs=1&pid=ImgDetMain&o=7&rm=3', collected: false },
-  { id: 9, name: 'Harry Kane', team: 'Inglaterra', photo: 'https://via.placeholder.com/150', collected: false },
+  { id: 9, name: 'Harry Kane', team: 'Inglaterra', photo: 'https://tse3.mm.bing.net/th/id/OIP.Uk-6lbQCxckIFvru78Yi8QHaKc?rs=1&pid=ImgDetMain&o=7&rm=3', collected: false },
   { id: 10, name: 'Son Heung-min', team: 'Coreia do Sul', photo: 'https://tse1.mm.bing.net/th/id/OIP.vfdqt02ElbkSoTM6efJjuwHaJ0?rs=1&pid=ImgDetMain&o=7&rm=3', collected: false },
 ];
